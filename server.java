83095d9077e1cf0da182c678e2c4e8d454b085eb
tacocats <jackjm22@outlook.com>
2016-06-13 16:28:50 -0400

    Added thread support, so each client gets it's own thread. Also added broadCast class to allow sending messages to all clients.

--- server.java
+++ server.java
@@ -1,60 +1,124 @@
-import org.lwjgl.system.CallbackI;
+// import org.lwjgl.system.CallbackI;
+
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.lang.*;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+// Used for sending information to all running clients
+class broadCast extends Thread{
+
+    // List of threads running/clients
+    public static ArrayList<Socket> socketList = new ArrayList<Socket>();
+
+    public void messageClients(String message) {
+
+        // Not needed anymore, threads are kept track of in an array
+        //Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        //Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
 
 
-public class server {
+        // Loops through the list of sockets running and sends the message to them
+        for (Socket i : socketList) {
+            try {
 
-    public server() {
+                PrintWriter out = new PrintWriter (i.getOutputStream(), true);
+                out.println(message);
+            } catch (IOException e) {
+                System.out.println(e.getStackTrace());
+            }
+        }
+    }
+}
 
-        boolean serverEnabled = true;
+// A client will be assigned a thread on the server
+class clientThread extends broadCast {
+    public Socket socket;
+    public InputStream input = null;
+    public BufferedReader reader = null;
+    //DataOutputStream out = null;
+    public PrintWriter out = null;
 
+    public clientThread (Socket clientSocket) {
+        this.socket = clientSocket;
+        System.out.println(this.socket);
+    }
+
+    public void run() {
         try {
-            ServerSocket srvr = new ServerSocket(2555);
-            Socket clientSocket = srvr.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            input = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(input));
+            //out = new DataOutputStream(socket.getOutputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            return;
+        }
 
-            String inputLine, outputLine;
-            // Server loop
-            while (serverEnabled == true) {
-                while ((inputLine = in.readLine()) != null) {
+        String inputLine, outputLine;
+
+        while (true) {
+            try {
+                while ((inputLine = reader.readLine()) != null) {
                     if (inputLine.equals("test") == true) {
                         System.out.println("I got the message " + inputLine);
                         out.println("echo back to client");
-                    }
-                    else if (inputLine.equals("close") == true) {
+
+                    } else if (inputLine.equals("close") == true) {
                         out.print("I got the test message");
                         System.out.println("closing the server");
-                        serverEnabled = false;
-                    }
-                    else {
+                    } else {
                         System.out.println("Error undefined message '" + inputLine + "'" + " sent from the client");
                         out.println("Error Undefined message");
                     }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
+    }
 
-            // Close the socket
-            clientSocket.close();
-            // Close the server
-            srvr.close();
+}
 
+public class server {
+
+    static final int port = 2555;
+
+    public server() {
+        boolean serverEnabled = true;
+        ServerSocket srvr = null;
+        Socket clientSocket = null;
+
+        try {
+            srvr = new ServerSocket(port);
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Something went wrong with starting the server");
+
+        }
+
+		while (serverEnabled == true) {
+            try {
+                clientSocket = srvr.accept();
+
+            } catch (IOException e) {
+
+                System.out.println("I/O error" + e);
+            }
+
+            // Create a new thread for the client
+            new clientThread(clientSocket).start();
+            broadCast.socketList.add(clientSocket);
         }
     }
 
     public static void main (String[] args) {
-        System.out.print("Attempting to start the server");
+        System.out.println("Attempting to start the server");
 
         // Create an instance of the server
         server gameServer = new server();
     }
-
 }
